0a68959dcdef3d5f6e870ed650746a305f936ea2
GYH <1490259042@qq.com>
2019-01-21 22:16:01 +0800

    city-list(better-scroll)

--- src/page/city/city.vue
+++ src/page/city/city.vue
@@ -1,15 +1,19 @@
 <template>
     <div>
         <city-header></city-header>
+        <city-list></city-list>
     </div>
 </template>
 
 <script>
-import cityHeader from './component/cityheader.vue'
+import cityHeader from './component/city-header'
+import cityList from './component/city-list'
+
 export default {
     name: "city",
     components: {
-        cityHeader
+        cityHeader,
+        cityList
     }
 }
 </script>
---
+++ src/page/city/component/city-list.vue
@@ -0,0 +1,149 @@
+<template>
+   <div class="list-wrap" ref = "wrapper"> 
+       <div>
+            <div class="area">
+                <div class="title border-topbottom">
+                    当前城市
+                    </div>
+                <ul class="wrap">
+                    <li class="wrap-item">北京</li>
+                </ul>
+            </div>
+            <div class="area">
+                <div class="title border-topbottom">
+                    热门城市
+                </div>
+                <ul class="wrap">
+                    <li class="wrap-item">北京</li>
+                    <li class="wrap-item">上海</li>
+                    <li class="wrap-item">天津</li>
+                    <li class="wrap-item">福建</li>  
+                    <li class="wrap-item">北京</li>
+                    <li class="wrap-item">上海</li>
+                    <li class="wrap-item">天津</li>
+                    <li class="wrap-item">福建</li>  
+                </ul>
+            </div>
+            <div class="area">
+                <div>
+                    <div class="title border-topbottom">
+                        A
+                    </div>
+                    <ul class="grapheme">
+                        <li class = "grap-item border-bottom">阿拉尔</li>
+                        <li class = "grap-item border-bottom">阿拉尔</li>
+                        <li class = "grap-item border-bottom">阿拉尔</li>
+                        <li class = "grap-item border-bottom">阿拉尔</li>
+                    </ul>
+                </div>
+                <div>
+                    <div class="title border-topbottom">
+                        A
+                    </div>
+                    <ul class="grapheme">
+                        <li class = "grap-item border-bottom">阿拉尔</li>
+                        <li class = "grap-item border-bottom">阿拉尔</li>
+                        <li class = "grap-item border-bottom">阿拉尔</li>
+                        <li class = "grap-item border-bottom">阿拉尔</li>
+                    </ul>
+                </div>
+                <div>
+                    <div class="title border-topbottom">
+                        A
+                    </div>
+                    <ul class="grapheme">
+                        <li class = "grap-item border-bottom">阿拉尔</li>
+                        <li class = "grap-item border-bottom">阿拉尔</li>
+                        <li class = "grap-item border-bottom">阿拉尔</li>
+                        <li class = "grap-item border-bottom">阿拉尔</li>
+                    </ul>
+                </div>
+                <div>
+                    <div class="title border-topbottom">
+                        A
+                    </div>
+                    <ul class="grapheme">
+                        <li class = "grap-item border-bottom">阿拉尔</li>
+                        <li class = "grap-item border-bottom">阿拉尔</li>
+                        <li class = "grap-item border-bottom">阿拉尔</li>
+                        <li class = "grap-item border-bottom">阿拉尔</li>
+                    </ul>
+                </div>
+                <div>
+                    <div class="title border-topbottom">
+                        A
+                    </div>
+                    <ul class="grapheme">
+                        <li class = "grap-item border-bottom">阿拉尔</li>
+                        <li class = "grap-item border-bottom">阿拉尔</li>
+                        <li class = "grap-item border-bottom">阿拉尔</li>
+                        <li class = "grap-item border-bottom">阿拉尔</li>
+                    </ul>
+                </div>
+                <div>
+                    <div class="title border-topbottom">
+                        A
+                    </div>
+                    <ul class="grapheme">
+                        <li class = "grap-item border-bottom">阿拉尔</li>
+                        <li class = "grap-item border-bottom">阿拉尔</li>
+                        <li class = "grap-item border-bottom">阿拉尔</li>
+                        <li class = "grap-item border-bottom">阿拉尔</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+   </div>
+</template>
+
+<script>
+import Bscroll from "better-scroll"
+export default {
+   name: "city-list",
+   mounted(){
+       this.scroll = new Bscroll(this.$refs.wrapper)
+   }
+}
+</script>
+
+<style lang="stylus" scoped>
+    .border-topbottom
+        &:after
+            border-color #cccccc
+        &:before
+            border-color #cccccc
+    .list-wrap
+        position absolute
+        top 1.7rem
+        left 0
+        right 0
+        bottom 0
+        overflow hidden
+        .title
+            line-height .54rem
+            background #eee
+            font-size .24rem
+            padding 0 .2rem
+        .wrap
+            background #fff
+            overflow hidden
+            padding .2rem .4rem .2rem .4rem
+            .wrap-item
+                float left
+                width 1.1rem
+                line-height .5rem
+                text-align center
+                border .02rem solid #666
+                margin .1rem
+        .grapheme
+            background #ffffff
+        .border-bottom
+            &:before
+                border-color #cccccc
+        .grap-item
+            box-sizing border-box
+            line-height .7rem
+            padding 0 .4rem
+
+
+</style>
