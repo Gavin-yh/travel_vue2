917de447c0d34baa58279a7a056bd567bffc0ab7
GYH <1490259042@qq.com>
2019-01-21 22:30:22 +0800

    city-graph

--- src/page/city/city.vue
+++ src/page/city/city.vue
@@ -2,18 +2,21 @@
     <div>
         <city-header></city-header>
         <city-list></city-list>
+        <city-graph></city-graph>
     </div>
 </template>
 
 <script>
 import cityHeader from './component/city-header'
 import cityList from './component/city-list'
+import cityGraph from './component/city-graph'
 
 export default {
     name: "city",
     components: {
         cityHeader,
-        cityList
+        cityList,
+        cityGraph
     }
 }
 </script>
---
+++ src/page/city/component/city-graph.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="wrap">
+       <ul class="wrap-item">
+           <li class="item">A</li>
+           <li class="item">B</li>
+           <li class="item">C</li>
+           <li class="item">D</li>
+       </ul> 
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="stylus" scoped>
+    .wrap-item
+        display flex
+        flex-direction column 
+        justify-content center
+        position absolute 
+        top 1.58rem
+        right 0
+        bottom 0
+        width .4rem
+        .item 
+            text-align center
+            line-height .44rem
+
+
+</style>
