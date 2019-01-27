c1e07677275c9263e562cdd256d4795f4f6dca8a
GYH <1490259042@qq.com>
2019-01-27 21:34:47 +0800

    state create new file and localStorage to save people's select for city

--- state/index.js
+++ state/index.js
@@ -1,15 +1,12 @@
 import vue from 'vue'
 import vuex from 'vuex'
+import city from './state'
+import changeState from './mutations'
 
 vue.use(vuex)
 
+
 export default new vuex.Store({
-    state: {
-        city :"上海"
-    },
-    mutations: {
-        changeState(state,city){
-            state.city = city
-        }
-    }
+    state: city,
+    mutations: changeState
 })
---
+++ state/state.js
@@ -0,0 +1,12 @@
+
+let Default = "上海"
+
+try{
+    if(localStorage.city){
+        Default = localStorage.city
+    }
+}catch (e){}
+
+export default  {
+    city : Default
+}
